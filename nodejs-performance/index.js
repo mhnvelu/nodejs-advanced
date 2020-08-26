fc18ed510a91d737331d2611a642a464ab7c4103
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-08-26 22:01:48 +0100

    NodeJS Clustering

--- nodejs-performance/index.js
+++ nodejs-performance/index.js
@@ -1,16 +1,33 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-const doWork = (duration) => {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
-};
+console.log(cluster.isMaster);
 
-// The request handlers are executed in event loop.
-// So, the event loop will block for 5 seconds below and it cant handle any other request, it cant perform any other operations
-app.get("/", (req, res, next) => {
-  doWork(5000);
-  res.status(200).send("Hi there !");
-});
+// Is the file being executed in master mode?
+if (cluster.isMaster) {
+  // This causes index.js to be executed again but in child mode
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  // This is child mode. It acts as server
+  const express = require("express");
+  const app = express();
+  const doWork = (duration) => {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  };
 
-app.listen(3000);
+  // The request handlers are executed in event loop.
+  // So, the event loop will block for 5 seconds below and it cant handle any other request, it cant perform any other operations
+  app.get("/", (req, res, next) => {
+    doWork(5000);
+    res.status(200).send("Hi there!");
+  });
+
+  app.get("/fast", (req, res, next) => {
+    res.status(200).send("This is fast!");
+  });
+
+  app.listen(3000);
+}
