c98ae48672c6c6dfb900e256962a4410021cf78f
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-02 21:51:38 +0100

    Implemented redis cache as hook in mongoose Query exec

--- blog-app-advanced/routes/blogRoutes.js
+++ blog-app-advanced/routes/blogRoutes.js
@@ -14,26 +14,8 @@ module.exports = (app) => {
   });
 
   app.get("/api/blogs", requireLogin, async (req, res) => {
-    const redis = require("redis");
-    const redisUrl = "redis://127.0.0.1:6379";
-    const client = redis.createClient(redisUrl);
-
-    // promisify converts a function which accepts callback into a function which returns promise.
-    const util = require("util");
-    client.get = util.promisify(client.get);
-
-    // Do we have any cached data in redis for this query?
-    const cachedBlogs = await client.get(req.user.id);
-    // If Yes, then respond to the request and return
-    if (cachedBlogs) {
-      console.log("Serving from cache");
-      return res.send(JSON.parse(cachedBlogs));
-    }
-    // If No, then respond to the request and update the cache to store the data
     const blogs = await Blog.find({ _user: req.user.id });
-    console.log("Serving from db");
     res.send(blogs);
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post("/api/blogs", requireLogin, async (req, res) => {
---
+++ blog-app-advanced/services/cache.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+
+// promisify converts a function which accepts callback into a function which returns promise.
+client.get = util.promisify(client.get);
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = async function () {
+  console.log("Mongoose Query exec hook for redis");
+
+  const redisKey = JSON.stringify({
+    ...this.getQuery(),
+    collection: this.mongooseCollection.name,
+  });
+  console.log(redisKey);
+
+  // Do we have any cached data in redis for this redisKey?
+  const cachedValue = await client.get(redisKey);
+  // If Yes, then respond to the request and return
+  if (cachedValue) {
+    console.log("Serving from cache");
+    // we need return mongoose doc from exec function. But we got json form redis cache
+
+    const doc = JSON.parse(cachedValue);
+
+    const result = Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+
+    return result;
+  }
+  // If No, then respond to the request and update the cache to store the data
+
+  const result = await exec.apply(this, arguments);
+  client.set(redisKey, JSON.stringify(result));
+  return result;
+};
