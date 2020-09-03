91e7d885de63cda43c5fb14134bb145c3545adf6
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-03 21:37:28 +0100

    Test case for checking the header text of page

--- blog-app-advanced/tests/header.test.js
+++ blog-app-advanced/tests/header.test.js
@@ -4,6 +4,11 @@ test("Launch a browser", async () => {
   const browser = await puppeteer.launch({
     headless: false,
   });
+  const page = await browser.newPage();
+  await page.goto("localhost:3000");
 
-  const page = browser.newPage();
+  // This code is serilazed as text, executed on chromium as JS and gets the result as text
+  const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
+  console.log(text);
+  expect(text).toEqual("Blogster");
 });
