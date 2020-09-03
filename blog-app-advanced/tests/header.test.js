595db68e258a6ab4bd0a7a44d61671eae360cd2d
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-09-03 21:53:48 +0100

    beforeEach, afterEach

--- blog-app-advanced/tests/header.test.js
+++ blog-app-advanced/tests/header.test.js
@@ -1,14 +1,23 @@
 const puppeteer = require("puppeteer");
 
-test("Launch a browser", async () => {
-  const browser = await puppeteer.launch({
+let browser, page;
+
+// Launch browser and page before each testcase
+beforeEach(async () => {
+  browser = await puppeteer.launch({
     headless: false,
   });
-  const page = await browser.newPage();
+  page = await browser.newPage();
   await page.goto("localhost:3000");
+});
 
+// Browser termination
+afterEach(async () => {
+  await browser.close();
+});
+
+test("Launch a browser", async () => {
   // This code is serilazed as text, executed on chromium as JS and gets the result as text
   const text = await page.$eval("a.brand-logo", (el) => el.innerHTML);
-  console.log(text);
   expect(text).toEqual("Blogster");
 });
